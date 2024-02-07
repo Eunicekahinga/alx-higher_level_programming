7a2546239cab26764edb4d691587613c7942e212
Eunicekahinga <euniwahito@gmail.com>
2024-02-07 20:19:05 +0300

    correction

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
+'''A square class based on 2-square.py'''
+
 
 class Square:
+    '''A private attribute int size'''
+
     def __init__(self, size=0):
+        '''Optional initialization of size, int'''
         self.size = size
 
     def area(self):
+        '''Returns current square area'''
         return (self.__size ** 2)
 
+    @property
     def size(self):
+        '''Retrieves private instance attribute, size'''
         return self.__size
 
+    @size.setter
     def size(self, value):
+        '''Property setter to set it'''
         if isinstance(value, int):
             if value >= 0:
                 self.__size = value
@@ -20,7 +30,7 @@ class Square:
             raise TypeError("size must be an integer")
 
     def my_print(self):
-        if self.__size = 0:
+        if self.__size == 0:
             print()
         else:
             for _ in range(self.__size):

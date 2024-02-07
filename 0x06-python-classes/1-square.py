55083fb8e166d4576e2ba58f82dd3a6899bb76a0
Eunicekahinga <euniwahito@gmail.com>
2024-02-07 19:16:51 +0300

    correction

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""A square that defines a square as in 0-square.py"""
+
 
 class Square:
+    """Size is a private attribute as its crucial in a square"""
     def __init__(self, size):
         self. __size = size

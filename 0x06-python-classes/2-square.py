b3c3f1378be3c85ee11357c1970606e1e3b5277b
Eunicekahinga <euniwahito@gmail.com>
2024-02-07 19:24:29 +0300

    correction

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""A square class defined as in 1-square.py"""
+
+
 class Square:
+    """Size is a private instance attribute and is an int"""
+
     def __init__ (self, size=0):
+        """Instantiation with optional size which is an int"""
         if isinstance(size, int):
             if size >= 0:
                 self.__size = size

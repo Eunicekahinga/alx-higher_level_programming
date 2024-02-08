c16644222bb202f005ace41b7c43abe14eb70d56
Eunicekahinga <euniwahito@gmail.com>
2024-02-08 14:27:46 +0300

    correction

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,6 +10,8 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
             except (TypeError, ValueError):
                 pass
+            except IndexError:
+                break
     finally:
         print()
         return count
